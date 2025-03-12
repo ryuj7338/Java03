82d1bac2bd3656906b5fb00269ca4aaad4105a7c
ryuj7338 <ryuj7338@gmail.com>
2025-03-12 11:16:48 +0900

    0312 extends 연습문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// extends => 상위 클래스(ex. 고양이)가 하위 클래스(ex.검은고양이, 흰 고양이)에게 상속
+// class 상위클래스명
+// class 하위클래스명 extends 상위클래스명
+// 중복을 최소화하기 위해 extends 사용
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+// 문제 : 아래와 같이 출력되도록 해주세요.
+// 조건 : `숨쉬다` 기능을 중복하지 말아주세요.
+
+class Main {
+    public static void main(String[] args) {
+        사람 a사람 = new 사람();
+        a사람.숨쉬다();
     }
+}
+class 동물 {
+    void 숨쉬다() {
+        System.out.println("숨쉬다.");
+    }
+}
+class 사람 extends 동물 {
+
 }
