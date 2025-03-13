49382cef8b108321c4214afa8369c5f64c2b4409
ryuj7338 <ryuj7338@gmail.com>
2025-03-13 10:32:43 +0900

    0313 형변환예시

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,111 +1,18 @@
 package org.example;
 
-// 문제 : 아래 코드가 실행되도록 해주세요.
-// 상황 : DVD방에 3개의 서로다른 tv가 있다. 영업을 하다보니 각 tv의 특별한 기능은 거의 사용될 일이 없고 기본적인 켜기/끄기 만 사용된다. 현재 리모콘이 3개나 있는습니다.
-// 문제 : 알바를 편하게 하기 위해서 표준Tv 리모콘을 구현해주세요.
+// 자바는 안전하다고 판단하면 자동으로 형 변환해준다.
+// complie - 다른 기계어로 변역하다. compiler - 번역가
+// 컴파일 타임   -   문법적 오류가 있다는걸 알려줌
+// 런타임  -   실행할 때 오류가 있다는걸 알려줌
 
 class Main {
     public static void main(String[] args) {
-        샤오미Tv a샤오미Tv = new 샤오미Tv();
-        삼성Tv a삼성Tv = new 삼성Tv();
-        LGTv aLGTv = new LGTv();
 
-        a샤오미Tv.켜기();
-        // 출력 => 샤오미Tv 켜집니다.
-        a샤오미Tv.끄기();
-        // 출력 => 샤오미Tv 꺼집니다.
-        a샤오미Tv.vr켜기();
-        // 출력 => 샤오미Tv vr켜기!
+//        float d = 5.0f;
+//        int i = d;  // 안전x, 자동 형 변환x
 
-        a삼성Tv.켜기();
-        // 출력 => 삼성Tv 켜집니다.
-        a삼성Tv.끄기();
-        // 출력 => 삼성Tv 꺼집니다.
-        a삼성Tv.ar켜기();
-        // 출력 => 삼성Tv ar켜기!
+        int i = ;
+        double d = i;
 
-        aLGTv.켜기();
-        // 출력 => LGTv 켜집니다.
-        aLGTv.끄기();
-        // 출력 => LGTv 꺼집니다.
-        aLGTv.게임모드전환();
-        // 출력 => LGTv 게임모드전환!
-
-        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
-
-        // 표준Tv 리모콘을 만든다.
-        표준Tv a표준Tv;
-
-        // a표준Tv 변수에 샤오미Tv 객체를 연결한다.
-        a표준Tv = a샤오미Tv;
-        a표준Tv.켜기();
-        // 출력 : 샤오기TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 샤오기TV 꺼집니다.
-
-        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
-        a표준Tv = a삼성Tv;
-        a표준Tv.켜기();
-        // 출력 : 삼성TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 삼성TV 꺼집니다.
-
-        // a표준Tv 변수에 LGTv 객체를 연결한다.
-        a표준Tv = aLGTv;
-        a표준Tv.켜기();
-        // 출력 : LGTV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : LGTV 꺼집니다.
-
-
-        // LGTV만의 고유 기능을 표준Tv 리모콘을 이용해서 호출하기 => 불가능
-        // (LGTv) => a표준Tv 변수에 있던 표준Tv 리모콘이 LGTv리모콘화 해서 `aLGTv2` 변수에 들어간다.
-        LGTv aLGTv2 = (LGTv)a표준Tv;
-        aLGTv2.게임모드전환();
     }
-}
-abstract class 표준Tv {
-   abstract void 켜기();
-   abstract void 끄기();
-
-}
-
-class 샤오미Tv extends 표준Tv{
-
-    void 켜기() {
-        System.out.println("샤오미Tv가 켜집니다.");
-    }
-    void 끄기() {
-        System.out.println("샤오미Tv가 꺼집니다.");
-    }
-    void vr켜기() {
-        System.out.println("샤오미Tv vr켜기!");
-    }
-
-}
-class 삼성Tv extends 표준Tv{
-
-    void 켜기() {
-        System.out.println("삼성Tv가 켜집니다.");
-    }
-    void 끄기() {
-        System.out.println("삼성Tv가 꺼집니다.");
-    }
-    void ar켜기() {
-        System.out.println("삼성Tv ar켜기!");
-    }
-
-}
-class LGTv extends 표준Tv{
-
-    void 켜기() {
-        System.out.println("LGTv가 켜집니다.");
-    }
-    void 끄기() {
-        System.out.println("LGTv가 꺼집니다.");
-    }
-    void 게임모드전환() {
-        System.out.println("LGTv 게임모드전환!");
-    }
-
 }
