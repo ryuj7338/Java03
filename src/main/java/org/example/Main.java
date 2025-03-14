697562adca579063da8efed804491561303b933d
ryuj7338 <ryuj7338@gmail.com>
2025-03-14 14:16:22 +0900

    0314 getter&setter 예시

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,46 +3,53 @@ package org.example;
 // stactic은 클래스 변수 , 모든 인스터스와 공유, 객체 생성과 무관하다.
 // this() -> 다른 생성자 호출(실행), Constructor call
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        사람 a사람 = new 사람();
 
-        int[] arr = new int[10];
+        a사람.setId(20);       // public 함수
+//        a사람.id = 20;    // private 함수를 사용하면 외부에서 내부 class 사람에 접근 x
 
-        Article[] articles = new Article[10];
-
-        Article article1 = new Article();
-        articles[0] = article1;
-
-        Article article2 = new Article();
-        articles[1] = article2;
-
-        System.out.println(articles[0].id);
-        System.out.println(articles[1].id);
-
-        System.out.println(article1.id);
-        System.out.println(article2.id);
+//        System.out.println("번호 : "+ a사람.id);
+        System.out.println("번호 : "+ a사람.getId());
     }
 }
 
-class Article {
-    static int lastId;
+class 사람 {
+    private int id;
+    private int age;
+    private String Name;
 
-    int id;
-    String regDate;
 
-    static {
-        lastId = 0;
+    public String getName() {
+        return Name;
     }
 
-    Article() {
-        this(lastId + 1, "2025-12-12 12:12:12"); // 다른 생성자 호출(실행), Constructor Call
-        lastId++;
+    public void setName(String name) {
+        Name = name;
     }
 
-    Article(int id, String regDate) {
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    
+    public void getId() {
+       return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
-        this.regDate = regDate;
     }
 }
+
 
 
