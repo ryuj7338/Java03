4696a08d213b8f10719b08c5c956ba3ac1ebae6f
ryuj7338 <ryuj7338@gmail.com>
2025-03-14 15:33:23 +0900

    0314 equals, ==

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,28 @@
 package org.example;
 
-// 유니코드
+// == : 주소값 비교
+// equals : String(참조타입)에서 사용
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println((int) '가');
-        System.out.println((int) '힣');
 
-        System.out.println(isHangeul('안'));
-        System.out.println(isHangeul('녕'));
-        System.out.println(isHangeul('A'));
-        System.out.println(isHangeul('.'));
-        System.out.println(isHangeul(' '));
+        String s1 = "하";
+        s1 += "하";
 
-    }
-    static boolean isHangeul(char c) {
-        return  c >= '가' && c <= '힣';
+        String s2 = "하하";
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+        System.out.println(s1 == s2);   // 연산자에 의한 비교 / 기본타입 비교는 가능 , 객체끼리 비교는 불가능
+        System.out.println(s1.equals(s2));  // equals라는 함수에 의한 비교(동등성 비교)   , 객체끼리 비교할떄 사용
+
+//        char c = '하';
+//
+//        char cc = 'a';
+//        cc = 'b';
+//
+//        String ss = "abc";  //  힙영역 -> abc
     }
 }
 
