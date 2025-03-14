b69d72a36b1d60aa25d468ee64b03efd1b1362bc
ryuj7338 <ryuj7338@gmail.com>
2025-03-14 14:51:25 +0900

    0314 try catch문

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,55 +1,51 @@
 package org.example;
 
-// stactic은 클래스 변수 , 모든 인스터스와 공유, 객체 생성과 무관하다.
-// this() -> 다른 생성자 호출(실행), Constructor call
-
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+// ex) 블루스크린 -> 오류
+// ex) 실행창이 뜨는 오류현상 -> 예외처리 가능
+// try ~ catch 문 - if 문처럼 해당되는 부분만 실행
 
 public class Main {
     public static void main(String[] args) {
-        사람 a사람 = new 사람();
 
-        a사람.setId(20);       // public 함수
-//        a사람.id = 20;    // private 함수를 사용하면 외부에서 내부 class 사람에 접근 x
+        try {
+            int rs =계산기.나누다(10, 0);
+            System.out.println(rs);
 
-//        System.out.println("번호 : "+ a사람.id);
-        System.out.println("번호 : "+ a사람.getId());
+        } catch (ArithmeticException e) {   // class 계산기 부분에서 예외처리 했기 떄문에 pass
+            System.out.println("0으로 못 나눠");
+        } catch (Exception e) { // class 계산기 부분에서 예외처리 했기 떄문에 pass
+            System.out.println("알 수 없는 예외 발생");
+        } finally {
+            System.out.println("어쨌든 실행");
+        }
+
+        System.out.println(123);
+
+
     }
 }
+class 계산기{
+    static int 나누다(int a, int b) throws ArithmeticException {   // throws 는 경고문 같은거 ex.개조심
+        int rs = 0;
 
-class 사람 {
-    private int id;
-    private int age;
-    private String Name;
+//        rs = a / b;
+
+        try {
+            rs = a / b;
+            return rs;
+        }catch (ArithmeticException e) {
+            System.out.println("try 중에 뭔가 잘못됨");
+            return 0;
+        }
+//        try {
+//            rs = a / b;
+//        }catch (ArithmeticException e) {
+//            System.out.println("try 중에 뭔가 잘못됨");
+//        }
 
 
-    public String getName() {
-        return Name;
-    }
 
-    public void setName(String name) {
-        Name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getAge() {
-        return age;
-    }
-    
-    public void getId() {
-       return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+ //       return  rs;
     }
 }
-
-
 
