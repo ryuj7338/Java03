02252fe53a5cdb3727872cbf25f74d4a6336a472
ryuj7338 <ryuj7338@gmail.com>
2025-03-17 14:39:44 +0900

    백준 10430

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,38 +2,21 @@ package org.example;
 
 // 키워드 - Object클래스, toString()
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        사람 a사람1 = new 사람("김철수", 22);
-        사람 a사람2 = new 사람("홍길동", 33);
 
-        System.out.println(a사람1);
-        System.out.printf("이름 : %s, 나이 : %d\n", a사람1.name, a사람1.age);
-        System.out.println(a사람2);
-        System.out.printf("이름 : %s, 나이 : %d\n", a사람2.name, a사람2.age);
+        Scanner sc = new Scanner(System.in);
+
+        int y;
+
+        y = sc.nextInt();
+
+        System.out.println(y-544);
 
     }
 }
-class 사람 extends Object {
-    String name;
-    int age;
 
-    @Override
-    public String toString() {
-        return "사람{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-
-    사람(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-
-
-}
 
 
